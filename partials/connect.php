b949f316340fdeba75fb752ee9622a7d991a5afc
Tanishq Verma <70758368+verma-tanishq@users.noreply.github.com>
2021-06-10 19:15:23 +0530

    Update connect.php

--- partials/connect.php
+++ partials/connect.php
@@ -1,9 +1,18 @@
 <?php
 
-$host="127.0.0.1:3307";
-$user="root";
-$password='';
-$dbname="phpstore";
+
+// for local database connection
+// $host="127.0.0.1:3307";
+// $user="root";
+// $password='';
+// $dbname="phpstore";
+
+
+// Remote database connection
+$host="remotemysql.com";
+$user="uL07pHLX3D";
+$password='5UOpf74RaZ';
+$dbname="uL07pHLX3D";
 
 $connect=mysqli_connect($host,$user,$password,$dbname);
 
